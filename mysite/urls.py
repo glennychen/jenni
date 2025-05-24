f624216e97083410419805ccebaa1fb8d0dd137b
glenn <glenn@asus.com>
2025-05-24 18:50:09 -0400

    Add about page

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import index
+from .views import index, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
     path("", index, name="index"),
+    path("about/", about, name="about"),    
     #path("pages/", include("pages.urls")),
 ]
