9cc43d788e273e0d7b73ced88bbddfd5e3ba6856
glenn <glenn@asus.com>
2025-05-25 12:07:15 -0400

    Map index to about page

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,7 +21,7 @@ from .views import index, about
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
-    path("", index, name="index"),
+    path("", about, name="index"),
     path("about/", about, name="about"),    
     #path("pages/", include("pages.urls")),
 ]
